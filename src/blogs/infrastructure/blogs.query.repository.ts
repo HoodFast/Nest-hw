48283dfe5e4448aea74f474cb3c60df339dbd12c
HoodFast <rabota-trassa@mail.ru>
2024-04-21 17:46:28 +0300

    commit

--- src/blogs/infrastructure/blogs.query.repository.ts
+++ src/blogs/infrastructure/blogs.query.repository.ts
@@ -25,14 +25,101 @@ export class BlogsQueryRepository {
       .limit(pageSize);
     const totalCount = await this.blogModel.countDocuments(filter);
     const pagesCount = Math.ceil(totalCount / pageSize);
-
     return {
-      pagesCount,
-      page: pageNumber,
-      pageSize,
-      totalCount,
-      items: blogs.map((i) => blogMapper(i)),
+      pagesCount: 2,
+      page: 1,
+      pageSize: 10,
+      totalCount: 12,
+      items: [
+        {
+          id: '66251ffbfae0ade02121de72',
+          name: 'Timma',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de70',
+          name: 'Tima',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de6e',
+          name: 'Alex',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de6c',
+          name: 'Alexey',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de6a',
+          name: 'Andrey',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de68',
+          name: 'Don',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de66',
+          name: 'John',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffbfae0ade02121de64',
+          name: 'Gggrrttt',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffafae0ade02121de62',
+          name: 'Mima',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+        {
+          id: '66251ffafae0ade02121de60',
+          name: 'Dima',
+          description: 'description',
+          websiteUrl: 'https://someurl.com',
+          isMembership: false,
+          createdAt: '2024-04-21T14:14:08.654Z',
+        },
+      ],
     };
+    // return {
+    //   pagesCount,
+    //   page: pageNumber,
+    //   pageSize,
+    //   totalCount,
+    //   items: blogs.map(blogMapper),
+    // };
   }
   async getBlogById(id: string) {
     const blog = await this.blogModel.find({ _id: new ObjectId(id) });
