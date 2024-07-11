54e60c9095ed1553034bde342880306334111f85
HoodFast <rabota-trassa@mail.ru>
2024-07-11 14:16:18 +0300

    commit

--- src/sessions/infrastructure/sessionQueryRepository.ts
+++ src/sessions/infrastructure/sessionQueryRepository.ts
@@ -1,25 +1,25 @@
-import { ObjectId } from 'mongodb';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Session, SessionDocument } from '../domain/session.schema';
-import { Injectable } from '@nestjs/common';
-import { JwtService } from '../../auth/infrastructure/jwt.service';
-import { SessionsOutputType } from '../api/output/session.output';
-import { sessionMapper } from '../domain/session.mapper';
-@Injectable()
-export class SessionRepository {
-  constructor(
-    @InjectModel(Session.name) private sessionModel: Model<SessionDocument>,
-    private jwtService: JwtService,
-  ) {}
-  async getAllSessions(token: string): Promise<SessionsOutputType[] | null> {
-    const metaData = await this.jwtService.getMetaDataByToken(token);
-    if (!metaData) return null;
-    const userId = metaData.userId;
-    const result = await this.sessionModel
-      .find({ userId: new ObjectId(userId) })
-      .lean();
-    if (!result) return null;
-    return sessionMapper(result);
-  }
-}
+// import { ObjectId } from 'mongodb';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Session, SessionDocument } from '../domain/session.schema';
+// import { Injectable } from '@nestjs/common';
+// import { JwtService } from '../../auth/infrastructure/jwt.service';
+// import { SessionsOutputType } from '../api/output/session.output';
+// import { sessionMapper } from '../domain/session.mapper';
+// @Injectable()
+// export class SessionRepository {
+//   constructor(
+//     @InjectModel(Session.name) private sessionModel: Model<SessionDocument>,
+//     private jwtService: JwtService,
+//   ) {}
+//   async getAllSessions(token: string): Promise<SessionsOutputType[] | null> {
+//     const metaData = await this.jwtService.getMetaDataByToken(token);
+//     if (!metaData) return null;
+//     const userId = metaData.userId;
+//     const result = await this.sessionModel
+//       .find({ userId: new ObjectId(userId) })
+//       .lean();
+//     if (!result) return null;
+//     return sessionMapper(result);
+//   }
+// }
