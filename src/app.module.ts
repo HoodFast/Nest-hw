436107de44a70eb51f6c912f4dc677a4a7cc714e
HoodFast <rabota-trassa@mail.ru>
2024-07-15 21:50:51 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { TestingController } from './testing/api/testing.controller';
 import { CommentsQueryRepository } from './comments/infrastructure/comments.query.repository';
 import { AuthService } from './auth/application/auth.service';
 import { JwtService } from './auth/infrastructure/jwt.service';
-import { SessionRepository } from './sessions/infrastructure/session.repository';
+// import { SessionRepository } from './sessions/infrastructure/session.repository';
 import { AuthController } from './auth/api/auth.controller';
 import { Session, SessionSchema } from './sessions/domain/session.schema';
 import { EmailService } from './auth/infrastructure/email.service';
@@ -48,11 +48,11 @@ import { CommentsController } from './comments/api/comments.controller';
 import { UpdateCommentBodyUseCase } from './comments/api/use-cases/update-comment-body.usecase';
 import { DeleteCommentUseCase } from './comments/api/use-cases/delete-comment.usecase';
 import { BlogExistsValidator } from './base/validate/blog.exist.validate';
-import { SessionQueryRepository } from './sessions/infrastructure/session.query.repository';
+// import { SessionQueryRepository } from './sessions/infrastructure/session.query.repository';
 import { SecurityController } from './sessions/api/security.controller';
-import { GetAllSessionUseCase } from './sessions/api/useCases/get-all-sessions.usecase';
-import { DeleteSessionByIdUseCase } from './sessions/api/useCases/delete-session-by-id.usecase';
-import { DeleteAllSessionsUseCase } from './sessions/api/useCases/delete-all-sessions.usecase';
+// import { GetAllSessionUseCase } from './sessions/api/useCases/get-all-sessions.usecase';
+// import { DeleteSessionByIdUseCase } from './sessions/api/useCases/delete-session-by-id.usecase';
+// import { DeleteAllSessionsUseCase } from './sessions/api/useCases/delete-all-sessions.usecase';
 
 const useCases = [
   CreateBlogUseCase,
@@ -67,9 +67,9 @@ const useCases = [
   UpdateCommentLikesUseCase,
   UpdateCommentBodyUseCase,
   DeleteCommentUseCase,
-  GetAllSessionUseCase,
-  DeleteSessionByIdUseCase,
-  DeleteAllSessionsUseCase,
+  // GetAllSessionUseCase,
+  // DeleteSessionByIdUseCase,
+  // DeleteAllSessionsUseCase,
 ];
 const repositories = [
   PostsRepository,
@@ -81,8 +81,8 @@ const repositories = [
   TestingQueryRepository,
   CommentsQueryRepository,
   CommentsRepository,
-  SessionRepository,
-  SessionQueryRepository,
+  // SessionRepository,
+  // SessionQueryRepository,
 ];
 const services = [
   AppService,
