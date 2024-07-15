b4ff9a0e9bff782565dde4b46cb72ffb71723476
HoodFast <rabota-trassa@mail.ru>
2024-07-15 21:17:00 +0300

    commit

--- src/sessions/infrastructure/session.query.repository.ts
+++ src/sessions/infrastructure/session.query.repository.ts
@@ -3,14 +3,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Session, SessionDocument } from '../domain/session.schema';
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '../../auth/infrastructure/jwt.service';
 import { sessionMapper } from '../domain/session.mapper';
 import { SessionsOutputType } from '../api/output/session.output';
 @Injectable()
 export class SessionQueryRepository {
   constructor(
     @InjectModel(Session.name) private sessionModel: Model<SessionDocument>,
-    private jwtService: JwtService,
   ) {}
   async getAllSessions(userId: string): Promise<SessionsOutputType[] | null> {
     const result = await this.sessionModel
