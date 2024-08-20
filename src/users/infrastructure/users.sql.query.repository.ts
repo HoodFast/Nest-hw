15912dbc4d565aabda25d33c44b4e8386cf52f9f
HoodFast <rabota-trassa@mail.ru>
2024-08-20 23:38:29 +0300

    commit

--- src/users/infrastructure/users.sql.query.repository.ts
+++ src/users/infrastructure/users.sql.query.repository.ts
@@ -3,7 +3,7 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
 @Injectable()
-export class UsersSqlRepository {
+export class UsersSqlQueryRepository {
   constructor(@InjectDataSource() protected dataSource: DataSource) {}
 
   async getAll(): Promise<any> {
