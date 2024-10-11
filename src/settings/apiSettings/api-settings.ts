ee02df92afb47109eaff4de46166c32cb48b9f90
HoodFast <rabota-trassa@mail.ru>
2024-10-11 11:49:45 +0300

    commit

--- src/settings/apiSettings/api-settings.ts
+++ src/settings/apiSettings/api-settings.ts
@@ -4,8 +4,7 @@ import { IsNumber, IsOptional, IsString } from 'class-validator';
 export class ApiSettings {
   constructor(private environmentVariables: EnvironmentVariable) {}
   @IsOptional()
-  @IsNumber()
-  PORT: number = Number(this.environmentVariables.PORT);
+  PORT = this.environmentVariables.PORT;
   @IsOptional()
   @IsString()
   LOCAL_HOST: string | undefined = this.environmentVariables.LOCAL_HOST;
